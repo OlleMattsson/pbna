a4d30e6cc131d53ea6303f997c986cea060cc47d
Olle Mattsson <olle.mattsson@gmail.com>
2023-11-15 11:35:10 +0200

    configure apollo client to be able to upload files to backend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,25 @@
 import { StrictMode } from "react";
-import App from "./App";
+import App from "./App2";
 import * as ReactDOMClient from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import createUploadLink from "apollo-upload-client/createUploadLink.mjs"
 
 const rootElement = document.getElementById("root") as Element;
 const root = ReactDOMClient.createRoot(rootElement);
 
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: createUploadLink({ 
+      uri: 'http://localhost:3000/api/graphql',
+      headers: {"Apollo-Require-Preflight": "true"}
+    })
+  })
+
 
 root.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
