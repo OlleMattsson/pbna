d183d70253bbce09cfc98bf961e97dffc69b31e0
Olle Mattsson <olle.mattsson@gmail.com>
2023-11-24 10:00:56 +0200

    handled by the keystone builder package

--- react-admin/src/buildQuery.js
+++ react-admin/src/buildQuery.js
@@ -11,48 +11,32 @@ const client = new ApolloClient({
 });
 
 const customizeBuildQuery = introspectionResults => (raFetchType, resourceName, params) => {
-   
     const builtQuery = buildQuery(introspectionResults)(raFetchType, resourceName, params);
 
-    switch (raFetchType){
-        case "GET_LIST":
-            return {
-                ...builtQuery,
-                parseResponse: response => {
-                    return {
-                        data: response.data.items,
-                        total: response.data.items.length
-                    }
-                }        
-            }
-           
-        case "GET_ONE": {
-            return {
-                ...builtQuery,
-                parseResponse: response => {
-                    return {
-                        data: response.data.data
-                    }
-                }        
-            }
-        }
-    }
+    // customizations go here
+
+    return builtQuery
+
 }
 
 export default buildGraphQLProvider({ 
     client: client, 
     buildQuery: customizeBuildQuery,
     introspection: {
-        /**
-         * the schema introspection expects list to have the format "item" -> "allItem"
-         * here, the builder is configured to use "items" 
-         */
         operationNames: {
-            "GET_LIST": resource => {
-                return `${resource.name.toLowerCase()}s`
-            },
+            /**
+             * keystone has all lowercase resource names
+             */
             "GET_ONE": resource => {
                 return `${resource.name.toLowerCase()}`
+            },
+
+            /**
+            * the schema introspection expects list to have the format "item" -> "allItem"
+            * here, the builder is configured to use "items" 
+            */
+            "GET_LIST": resource => {
+                return `${resource.name.toLowerCase()}s`
             }
         }
     }
