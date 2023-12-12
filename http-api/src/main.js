f7be27b4f91bf96c4569474d1dd3afcf5556d834
Olle Mattsson <olle.mattsson@gmail.com>
2023-12-12 23:44:38 +0200

    http-api: add logic for setting the password for a new profile

--- http-api/src/main.js
+++ http-api/src/main.js
@@ -5,6 +5,13 @@ import { Message } from 'redis-smq';
 import {config as smqConfig, queueNames} from "../common/redis-smq-config.js"
 import { sendMessage } from '../common/smqSendMessage.js';
 import cors from 'cors'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core/core.cjs';
+import { HttpLink } from "@apollo/client/link/http/http.cjs";
+import { ApolloLink } from "@apollo/client/link/core/ApolloLink.js"
+import util from 'util'
+import { keystoneAuth, authenticatedClient } from "./keystoneAuth.js";
+
+
 
 /**
  * Start server
@@ -35,10 +42,7 @@ server.listen(port, () => {
 
 httpApi.post('/verifyemail', (req, res) => {
 
-  console.log(req.body)
-
   const { emailAddress } = req.body;
-
 
   // create a message for redis SMQ
   const message = new Message();
@@ -51,6 +55,76 @@ httpApi.post('/verifyemail', (req, res) => {
   
   res.send("ok")
 });
+
+httpApi.post('/createprofile', async (req, res) => {
+  const {email, password, invitationToken} = req.body
+  const client = authenticatedClient(await keystoneAuth())
+
+  try {
+    const response = await client.query({
+      query: gql`
+        query Query($where: UserWhereUniqueInput!) {
+          user(where: $where) {
+            invitationToken
+          }
+        }
+      `,
+      variables: {
+        where: {
+          email
+        }
+      }
+    })
+
+    const invitationTokenChallange = response.data.user.invitationToken
+
+    if (invitationToken === invitationTokenChallange) {
+      client.mutate({
+        mutation: gql`
+          mutation Mutation($where: UserWhereUniqueInput!, $data: UserUpdateInput!) {
+            updateUser(where: $where, data: $data) {
+              id
+            }
+          }        
+        `,
+        variables: {
+          where: {
+            email
+          },
+          data: {
+            password,
+            // reset the invitation token so that password can only be set once
+            invitationToken: "null" 
+          }
+        }
+      }).then(r => {
+        res.status(200).send({message: "password set"})
+      }).catch(e => {
+        console.log(e)
+      })
+
+    } else {
+      console.log("token fail")
+      res.status(401).send({message: 'invitation token mismatch'})
+    }
+
+  } catch(e) {
+    res.status(404).send({message: "user does not exist"})
+  }
+
+
+
+
+  // 2. set the password in keystone
+  // 3. send a welcome email to the new user
+
+
+  //
+
+
+
+})
+
 
 /**
  * Define socket events
