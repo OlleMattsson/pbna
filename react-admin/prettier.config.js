6148dd22dd717358c2042580c54d3474f7d7eb7c
Olle Mattsson <olle.mattsson@gmail.com>
2025-09-20 04:08:14 +0300

    add prettier config

--- react-admin/prettier.config.js
+++ react-admin/prettier.config.js
@@ -1,1 +1,14 @@
-module.exports = {}
+/**
+ * Shared Prettier configuration for the React Admin frontend.
+ * Keep it in sync with any editor integrations so on-save formatting matches CLI output.
+ */
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
