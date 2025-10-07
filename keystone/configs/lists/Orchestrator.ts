ae1b9a962b793e91493e81b1640dc6d58e8b9243
Olle Mattsson <olle.mattsson@gmail.com>
2025-10-07 14:51:57 +0300

    run orchestrator manually from keystone admin ui

--- keystone/configs/lists/Orchestrator.ts
+++ keystone/configs/lists/Orchestrator.ts
@@ -2,6 +2,7 @@ import { list } from "@keystone-6/core";
 import { text, select, relationship, checkbox } from "@keystone-6/core/fields";
 import { allowAll } from "@keystone-6/core/access";
 import { INVOICE_UPDATE_STATUS_VERIFIED } from "../../events/invoiceUpdateStatusVerified";
+import { runOrchestrator } from "../../helpers/orchestrator";
 
 export const Orchestrator = list({
   access: allowAll,
@@ -45,5 +46,37 @@ export const Orchestrator = list({
       },
     }),
     isEnabled: checkbox({ defaultValue: true }),
+
+    // Run orchestrator manually, useful for debugging
+    runOrchestrator: select({
+      options: [
+        { label: "None", value: "none" },
+        { label: "Run Orchestrator", value: "run" },
+      ],
+      defaultValue: "none",
+      ui: {
+        displayMode: "segmented-control", // or 'select'
+      },
+      hooks: {
+        afterOperation: async ({ operation, item, context }) => {
+          if (operation === "delete") return;
+
+          if (item.runOrchestrator === "run") {
+            // reset the action field to avoid re- triggering
+            await context.db.Orchestrator.updateOne({
+              where: { id: item.id },
+              data: { runOrchestrator: "none" },
+            });
+
+            // run orchestrators
+            await runOrchestrator({
+              orchestratorId: item.id,
+              contextMap: {},
+              context,
+            });
+          }
+        },
+      },
+    }),
   },
 });
